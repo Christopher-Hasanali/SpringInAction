2e9bd76566a93393b5aa3d62525a1f94155f5af2
Christopher Hasanali <christopher.hasanali@gmail.com>
2017-11-24 17:45:35 -0500

    Added AOP

--- src/com/hasanac/springinaction/knights/BraveKnight.java
+++ src/com/hasanac/springinaction/knights/BraveKnight.java
@@ -5,11 +5,15 @@ package com.hasanac.springinaction.knights;
  */
 public class BraveKnight implements Knight {
     private Quest quest;
-    public BraveKnight(Quest quest) {
+    private Minstrel minstrel;
+    public BraveKnight(Quest quest, Minstrel minstrel) {
         this.quest = quest;
+        this.minstrel = minstrel;
     }
 
     public void embarkOnQuest() {
+        minstrel.singBeforeQuest();
         quest.embark();
+        minstrel.singAfterQuest();
     }
 }
---
+++ src/com/hasanac/springinaction/knights/Minstrel.java
@@ -0,0 +1,19 @@
+package com.hasanac.springinaction.knights;
+
+import java.io.PrintStream;
+
+public class Minstrel {
+    private PrintStream stream;
+
+    public Minstrel(PrintStream stream) {
+        this.stream = stream;
+    }
+
+    public void singBeforeQuest() {
+        stream.println("Before Quest");
+    }
+
+    public void singAfterQuest() {
+        stream.println("After Quest");
+    }
+}
